1b2443097c09ac2f2bd782e962252b8546363206
adityaradhakrishnan <adityaradhakrishnan@gmail.com>
2015-01-22 22:21:04 -0500

    Turns out, the minimal HTML5 document is not quite as minimal as I had originally surmised. Modified PubMed-Parse.py accordingly to reflect that. This, however, is the smallest valid and W3C compliant HTML5 document you can code.

--- PubMed-Parse.py
+++ PubMed-Parse.py
@@ -8,8 +8,12 @@ import urllib2 as url
 pubHTML = open('Publications.html','w')
 
 pubHTML.write("<!DOCTYPE html>\n")
+pubHTML.write("<html lang=en>\n")
+pubHTML.write("<head>\n")
 pubHTML.write("<meta charset=utf-8>\n")
 pubHTML.write("<title>Publication List</title>\n")
+pubHTML.write("</head>\n")
+pubHTML.write("<body>\n")
 
 #
 # Technically you don't have to close the file
@@ -17,4 +21,6 @@ pubHTML.write("<title>Publication List</title>\n")
 # of Python, but it's just good coding practice.
 #
 
+pubHTML.write("</body>\n")
+pubHTML.write("</html>\n")
 pubHTML.close()
